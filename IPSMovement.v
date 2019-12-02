eaab4287b1bf891782aee7d5761414feae272faf
Rice Rodriguez <rice.rodriguez@ttu.edu>
2019-12-02 14:47:57 -0600

    Added an inner state machine

--- IPSMovement.v
+++ IPSMovement.v
@@ -19,19 +19,22 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-module IPSMovement(
-    input             clk,
-    input [2:0]       IPS,
-    input             IR,
-    input             IRModuleDone,
-    output reg [3:0]  Direction,
-    output reg [11:0] DutyA, DutyB,
-    output reg        EnableIRModule,
-    output reg        ResetIRModule
-    );
+module IPSMovement
+  (input             clk,
+   input [2:0]       IPS,
+   input             IR,
+   input             IRModuleDone,
+   output reg [3:0]  Direction,
+   output reg [11:0] DutyA, DutyB,
+   output reg        EnableIRModule,
+   output reg        ResetIRModule
+   );
 
-   reg [1:0]         state; // 0 means line following (with IR), 1 means line following (ignore IR until it hits 0), 2 means move servos
+   reg [1:0]         state_outer; // 0 means line following (with IR), 1 means line following (ignore IR until it hits 0), 2 means move servos
+   reg [2:0]         state_inner;
+
    localparam LINE_FOLLOW_IR = 0, LINE_FOLLOW_NO_IR = 1, MOVE_SERVOS = 2;
+   localparam FWD = 0, LEFT = 1, RIGHT = 2, BWD = 3, STOP = 4;
 
    initial 
      begin
@@ -42,48 +45,181 @@ module IPSMovement(
      end        
 
    always @ (posedge clk)
-     case (state)
+     case (state_outer)
        LINE_FOLLOW_IR, LINE_FOLLOW_NO_IR:
          begin
             ResetIRModule<=0;
-            if (state == LINE_FOLLOW_IR && IR)
-              state <= MOVE_SERVOS;
+            if (state_outer == LINE_FOLLOW_IR && IR)
+              state_outer <= MOVE_SERVOS;
 
-            else if (state == LINE_FOLLOW_NO_IR && !IR)
-              state <= LINE_FOLLOW_IR;
+            else if (state_outer == LINE_FOLLOW_NO_IR && !IR)
+              state_outer <= LINE_FOLLOW_IR;
 
-            casex (IPS) 
-              3'b111:     //all sensors detected, stop 
-                begin
-                   Direction <= 4'b0110;
-                   {DutyA,DutyB} <= {12'd0, 12'd0};
-                end
+            case (state_inner)
+              FWD:
+                casex (IPS) 
+                  3'b111:     //all sensors detected, stop 
+                    begin
+                       state_inner <= STOP;
+                       Direction <= 4'b0;
+                       {DutyA,DutyB} <= {12'd0, 12'd0};
+                    end
 
-              3'b010 :  //middle IPS detected, go straight
-                begin
-                   Direction <= 4'b0110;
-                   {DutyA,DutyB} <= {12'd4000, 12'd4000};
-                end
+                  3'b010 :  //middle IPS detected, go straight
+                    begin
+                       state_inner <= FWD;
+                       Direction <= 4'b0110;
+                       {DutyA,DutyB} <= {12'd4000, 12'd4000};
+                    end
 
-              3'b1x0: //LEFt or left and middle IPS detected, pivot left
-                begin
-                   Direction <= 4'b0101;
-                   {DutyA,DutyB} <= {12'd3000, 12'd3000};
-                end
-              
-              3'b0x1 : //Right or Right and middle IPS detected, pivot right
-                begin
-                   Direction <= 4'b1010;
-                   {DutyA,DutyB} <= {12'd3000, 12'd3000};    
-                end
+                  3'b1x0: //LEFt or left and middle IPS detected, pivot left
+                    begin
+                       state_inner <= LEFT;
+                       Direction <= 4'b0101;
+                       {DutyA,DutyB} <= {12'd3000, 12'd3000};
+                    end
+                  
+                  3'b0x1 : //Right or Right and middle IPS detected, pivot right
+                    begin
+                       state_inner <= RIGHT;
+                       Direction <= 4'b1010;
+                       {DutyA,DutyB} <= {12'd3000, 12'd3000};    
+                    end
+
+                  default:
+                    begin
+                       state_inner <= BWD;
+                       Direction <= 4'b1001; //Backwards direction to get back on track
+                       {DutyA,DutyB} <= {12'd4000,12'd4000};
+                    end
+                endcase // casex (IPS)
+
+              LEFT:
+                casex (IPS) 
+                  3'b111:     //all sensors detected, KEEP TURNING LEFT
+                    begin
+                       state_inner <= LEFT;
+                       Direction <= 4'b0101;
+                       {DutyA,DutyB} <= {12'd3000, 12'd3000};
+                    end
+
+                  3'b010 :  //middle IPS detected, go straight
+                    begin
+                       state_inner <= FWD;
+                       Direction <= 4'b0110;
+                       {DutyA,DutyB} <= {12'd4000, 12'd4000};
+                    end
+
+                  3'b1x0: //LEFt or left and middle IPS detected, pivot left
+                    begin
+                       state_inner <= LEFT;
+                       Direction <= 4'b0101;
+                       {DutyA,DutyB} <= {12'd3000, 12'd3000};
+                    end
+                  
+                  3'b0x1 : //Right or Right and middle IPS detected, pivot right
+                    begin
+                       state_inner <= RIGHT;
+                       Direction <= 4'b1010;
+                       {DutyA,DutyB} <= {12'd3000, 12'd3000};    
+                    end
+
+                  default:
+                    begin
+                       state_inner <= LEFT;
+                       Direction <= 4'b0101;
+                       // state_inner <= BWD;
+                       // Direction <= 4'b1001; //Backwards direction to get back on track
+                       {DutyA,DutyB} <= {12'd4000,12'd4000};
+                    end
+                endcase // casex (IPS)
+
+              RIGHT:
+                casex (IPS) 
+                  3'b111:     //all sensors detected, KEEP TURNING LEFT
+                    begin
+                       state_inner <= BWD;
+                       Direction <= 4'b1001; //Backwards direction to get back on track
+                       {DutyA,DutyB} <= {12'd3000, 12'd3000};
+                    end
+
+                  3'b010 :  //middle IPS detected, go straight
+                    begin
+                       state_inner <= FWD;
+                       Direction <= 4'b0110;
+                       {DutyA,DutyB} <= {12'd4000, 12'd4000};
+                    end
+
+                  3'b1x0: //LEFt or left and middle IPS detected, pivot left
+                    begin
+                       state_inner <= LEFT;
+                       Direction <= 4'b0101;
+                       {DutyA,DutyB} <= {12'd3000, 12'd3000};
+                    end
+                  
+                  3'b0x1 : //Right or Right and middle IPS detected, pivot right
+                    begin
+                       state_inner <= RIGHT;
+                       Direction <= 4'b1010;
+                       {DutyA,DutyB} <= {12'd3000, 12'd3000};    
+                    end
+
+                  default:
+                    begin
+                       state_inner <= RIGHT;
+                       Direction <= 4'b1010;
+                       // state_inner <= BWD;
+                       // Direction <= 4'b1001; //Backwards direction to get back on track
+                       {DutyA,DutyB} <= {12'd4000,12'd4000};
+                    end
+                endcase // casex (IPS)
+
+              BWD:
+                casex (IPS) 
+                  // 3'b111:     //all sensors detected, KEEP TURNING LEFT
+                  //   begin
+                  //      state_inner <= RIGHT;
+                  //      Direction <= 4'b0101;
+                  //      {DutyA,DutyB} <= {12'd3000, 12'd3000};
+                  //   end
+
+                  3'b010 :  //middle IPS detected, go straight
+                    begin
+                       state_inner <= FWD;
+                       Direction <= 4'b0110;
+                       {DutyA,DutyB} <= {12'd4000, 12'd4000};
+                    end
+
+                  3'b1x0: //LEFt or left and middle IPS detected, pivot left
+                    begin
+                       state_inner <= LEFT;
+                       Direction <= 4'b0101;
+                       {DutyA,DutyB} <= {12'd3000, 12'd3000};
+                    end
+                  
+                  3'b0x1 : //Right or Right and middle IPS detected, pivot right
+                    begin
+                       state_inner <= RIGHT;
+                       Direction <= 4'b1010;
+                       {DutyA,DutyB} <= {12'd3000, 12'd3000};    
+                    end
+
+                  default:
+                    begin
+                       state_inner <= BWD;
+                       Direction <= 4'b1001; //Backwards direction to get back on track
+                       {DutyA,DutyB} <= {12'd3000, 12'd3000};
+                    end
+                endcase // casex (IPS)
 
               default:
                 begin
-                   Direction <= 4'b1001; //Backwards direction to get back on track
-                   {DutyA,DutyB} <= {12'd4000,12'd4000};
-                end
-            endcase
-         end
+                   state_inner <= STOP;
+                   Direction <= 4'b0;
+                   {DutyA,DutyB} <= 24'd0;
+                end  
+            end // case: LINE_FOLLOW_IR, LINE_FOLLOW_NO_IR
+       
 
        MOVE_SERVOS: // State 2 for moving servos
          begin
@@ -97,10 +233,10 @@ module IPSMovement(
               begin
                  EnableIRModule <= 0;
                  ResetIRModule <= 1;
-                 state <= LINE_FOLLOW_NO_IR;
+                 state_outer <= LINE_FOLLOW_NO_IR;
               end
          end
-       
-       default: state<=LINE_FOLLOW_IR;
-     endcase
-endmodule
+              
+              default: state_outer<=LINE_FOLLOW_IR;
+            endcase
+            endmodule
